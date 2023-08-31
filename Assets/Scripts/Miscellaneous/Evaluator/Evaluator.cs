b3f390362b0f6fb3346425e402c63650ae3af11a
Thev2Andy <ciprian.andrei002@gmail.com>
2023-08-31 19:18:20 +0300

    Updated the expression evaluator.

--- Assets/Scripts/Miscellaneous/Evaluator/Evaluator.cs
+++ Assets/Scripts/Miscellaneous/Evaluator/Evaluator.cs
@@ -8,20 +8,7 @@ using UnityEngine;
 public static class Evaluator
 {
     public static string Evaluate(string Expression, IContext Context){
-        return Evaluator.EvaluateBooleanExpressions(Evaluator.EvaluateMathematicalExpressions(Expression, Context).ToString());
-    }
-
-
-    public static string EvaluateMathematicalExpressions(string Expression, IContext Context) {
-        return Regex.Replace(Expression, @"\^(.+?)\^", (Match Match) => {
-            return Parser.Parse(Match.Groups[1].Value, Context).ToString();
-        });
-    }
-
-    public static string EvaluateBooleanExpressions(string Expression) {
-        return Regex.Replace(Expression, @"\$(.*?)\$", (Match Match) => {
-            return Converter.AsIntegerStringFromBoolean(BooleanEvaluator.Evaluate(Match.Groups[1].Value));
-        });
+        return Parser.Parse(Expression, Context).ToString();
     }
 
 
